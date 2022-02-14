0b77485641e9fb3099997580d6e53465b196f5f0
BlueTree242 <yahiamsm2@gmail.com>
2022-02-14 22:11:20 +0200

    add break line in one of javadocs in EventHandler

--- src/main/java/me/bluetree242/jdaeventer/EventHandler.java
+++ src/main/java/me/bluetree242/jdaeventer/EventHandler.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.events.GenericEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * This class is event handler, you can add event handlers to {@link JDAEventer#addEventHandler(EventHandler)} and it will call the {@link EventHandler#onEvent(GenericEvent)} if the events match
+ * This class is event handler, you can add event handlers to {@link JDAEventer#addEventHandler(EventHandler)} and it will call the {@link EventHandler#onEvent(GenericEvent)} if the events match <br>
  * Example Event Handler is the {@link me.bluetree242.jdaeventer.impl.MethodEventHandler} which is one per method when you add a {@link DiscordListener}
  * @see JDAEventer#addEventHandler(EventHandler)
  * @param <T> event type
