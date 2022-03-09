34879b063103ba18b44c479ee1a0f1cf5f78721d
Blue Tree <69328929+BlueTree242@users.noreply.github.com>
2022-03-09 21:34:01 +0200

    fix bad bug that you cant register events because it thinks all events arent events and  vice versa

--- src/main/java/me/bluetree242/jdaeventer/impl/MethodEventHandler.java
+++ src/main/java/me/bluetree242/jdaeventer/impl/MethodEventHandler.java
@@ -54,7 +54,7 @@ public class MethodEventHandler implements EventHandler {
         if (annot == null) throw new IllegalStateException("Bad Method to register");
         if (method.getParameterCount() > 2)
             throw new BadListenerException("@HandleEvent on a method with more than 2 parameters");
-        if (GenericEvent.class.isAssignableFrom(method.getParameterTypes()[0]))
+        if (!GenericEvent.class.isAssignableFrom(method.getParameterTypes()[0]))
             throw new BadListenerException("Method " + method.toGenericString() + " first parameter is not an event");
         if (method.getParameterCount() == 2)
             if (!EventInformation.class.isAssignableFrom(method.getParameterTypes()[1]))
