12001ec0c11f93db1a62a85637614497c2a8601f
BlueTree242 <yahiamsm2@gmail.com>
2022-03-12 19:06:47 +0200

    commit changes in connection before closing it

--- src/main/java/me/bluetree242/jdaeventer/JDAEventer.java
+++ src/main/java/me/bluetree242/jdaeventer/JDAEventer.java
@@ -179,6 +179,7 @@ public class JDAEventer {
         }
         if (info.isConnectionOpen()) {
             try {
+                info.getConnection().commit();
                 info.getConnection().close();
             } catch (SQLException ex) {
                 ex.printStackTrace();
